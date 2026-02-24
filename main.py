bc4a46dadd5687b4ef50ea6e1f4f5f14466a508c
syurein <127266308+syurein@users.noreply.github.com>
2026-02-25 02:55:37 +0900

    add

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, request
 import json
 
 app=Flask(__name__)
@@ -8,7 +8,33 @@ def get_html():
         text=f.read()
         print(f.read())
     return text
+    
+@app.route('/get_data')
+def get_data():
+    try:
+        with open('./data.json','r',encoding='utf-8') as f:
+            context=f.read()
+        return context
+    except FileNotFoundError:
+        return '{}'
 
+@app.route('/set_data',methods=["POST"])
+def set_data():
+  
+    datas=json.loads(request.get_data())
+    
+    with open('./data.json','r',encoding='utf-8') as f:
+        
+        data=json.load(f)
+        print(data[datas['title']]['status'])
+        if data[datas['title']]:
+            print(data[datas['title']]['status'])
+            data[datas['title']]['status']=datas['status']
+
+    with open('./data.json','w',encoding='utf-8') as f:
+        json.dump(data,f,indent=2)
+        print('success')
+    return '{status:"success"}'
 
 if __name__ == "__main__":
 	app.run(debug=True)
