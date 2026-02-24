2f97827b98cbd2904d5959ebeef2fa19f3c9fa1d
syurein <127266308+syurein@users.noreply.github.com>
2026-02-25 04:32:27 +0900

    add

--- main.py
+++ main.py
@@ -17,7 +17,24 @@ def get_data():
         return context
     except FileNotFoundError:
         return '{}'
+@app.route('/search_data/<searchword>')
+def search_data(searchword):
+    
+    finalize={}#構造体として宣言しとく
+    i=0
+    try:
+        with open('./data.json','r',encoding='utf-8') as f:
+            context=json.load(f)
+            text=list(context.keys())
+            for content in text:
+                if searchword in content:
+                    finalize[str(text[i])]=context[str(text[i])]
+                    #構造体にしたらappend使わなくていいから！
+                i+=1
 
+        return finalize
+    except FileNotFoundError:
+        return '{}'
 @app.route('/set_data',methods=["POST"])
 def set_data():
   
@@ -35,6 +52,25 @@ def set_data():
         json.dump(data,f,indent=2)
         print('success')
     return '{status:"success"}'
+@app.route('/set_borrow',methods=["POST"])
+def set_borrow():
+  
+    datas=json.loads(request.get_data())
+    
+    with open('./data.json','r',encoding='utf-8') as f:
+        
+        data=json.load(f)
+        print(data[datas['title']]['name'])
+        if data[datas['title']]:
+            print(data[datas['title']]['status'])
+            data[datas['title']]['status']=datas['status']
+            data[datas['title']]['name']=datas['name']
+
+    with open('./data.json','w',encoding='utf-8') as f:
+        json.dump(data,f,indent=2)
+        print('success')
+    return '{status:"success"}'
 
 if __name__ == "__main__":
-	app.run(debug=True,host='0.0.0.0', port=os.environ["PORT"])
+	#app.run(debug=True,host='0.0.0.0', port=os.environ["PORT"])
+    app.run(debug=True,host='0.0.0.0', port=7860)
